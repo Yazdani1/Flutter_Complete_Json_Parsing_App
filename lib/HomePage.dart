44924a3fa7bd2df7ca6b1fe59577669e6f1ccad4
Yazdani <yaz4dani@gmail.com>
2019-05-18 16:28:34 +0600

    Second

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,4 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'Model/Data.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class Home extends StatefulWidget {
   @override
@@ -6,6 +12,31 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  Future<List<Data>> _getData() async{
+
+    var mainUrl="https://jsonplaceholder.typicode.com/photos";
+
+    var data = await http.get(mainUrl);
+    var jsonData= json.decode(data.body);
+
+    List<Data>listOf=[];
+
+    for (var i in jsonData){
+      Data data=new Data(i['id'], i['title'], i["url"], i["thumbnailUrl"]);
+      listOf.add(data);
+    }
+    return listOf;
+
+  }
+
+  //random color genarator
+
+  List<MaterialColor>_color=[Colors.orange,Colors.green,Colors.blue,Colors.purple,Colors
+  .pink,Colors.amber];
+
+
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -58,13 +89,115 @@ class _HomeState extends State<Home> {
               onTap: (){
                 Navigator.of(context).pop();
               },
+            ),
+            new ListTile(
+              title: new Text("Fourth Page"),
+              leading: new Icon(Icons.edit,color: Colors.brown,),
+              onTap: (){
+                Navigator.of(context).pop();
+              },
+            ),
+            new ListTile(
+              title: new Text("Fifth Page"),
+              leading: new Icon(Icons.print,color: Colors.yellow,),
+              onTap: (){
+                Navigator.of(context).pop();
+              },
+            ),
+            new Divider(
+             height: 5.0,
+              color: Colors.black,
+            ),
+            new ListTile(
+              title: new Text("Close"),
+              leading: new Icon(Icons.close,color: Colors.red,),
+              onTap: (){
+                Navigator.of(context).pop();
+              },
             )
 
           ],
         ),
       ),
 
+      body: new ListView(
+        children: <Widget>[
+          
+          new Container(
+            margin: EdgeInsets.all(10.0),
+            height: 250.0,
+            child: new FutureBuilder(
+                future: _getData(),
+              builder: (BuildContext c,AsyncSnapshot snapshot){
+                  
+                  if(snapshot.data==null){
+                    return Center(
+                      child: new Text("Loading..."),
+                    );
+                  }else{
+                    return ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: snapshot.data.length,
+                      itemBuilder: (BuildContext c,int index){
+                        MaterialColor color=_color[index % _color.length];
+                        return Container(
+                          width: 150.0,
+                          child: new Card(
+                            elevation: 10.0,
+                            child: new Column(
+                              children: <Widget>[
 
+                                new Image.network(snapshot.data[index].url,
+                                height: 150.0,
+                                  width: 150.0,
+                                  fit: BoxFit.cover,
+                                ),
+                                new SizedBox(height: 5.0,),
+                                new Container(
+                                  child: new Row(
+                                    children: <Widget>[
+                                      
+                                      Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: new CircleAvatar(
+
+                                          backgroundColor: color,
+                                          child: new Text(snapshot.data[index].id.toString()),
+                                          foregroundColor: Colors.white,
+                                        ),
+                                      ),
+                                      new SizedBox(width: 5.0,),
+                                      Container(
+                                        width: 60.0,
+                                        child: new Text(snapshot.data[index].title,
+                                          maxLines: 1,
+                                          style: TextStyle(
+                                            fontSize: 22.0,
+                                            color: Colors.orange
+                                          ),
+                                        ),
+                                      )
+                                      
+                                    ],
+                                  ),
+                                )
+                     
+
+                              ],
+                            ),
+                          ),
+                        );
+                      }
+                    );
+                  }
+                  
+              }
+            ),
+          ),//end container....
+          
+          
+        ],
+      )
 
     );
   }
---
+++ lib/Model/Data.dart
@@ -0,0 +1,18 @@
+class Data{
+
+  int id;
+  String title;
+  String url;
+  String thumbnailUrl;
+
+  Data(
+
+      this.id,
+      this.title,
+      this.url,
+      this.thumbnailUrl
+
+      );
+
+
+}
